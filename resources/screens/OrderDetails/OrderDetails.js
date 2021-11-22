15bb9319ccd986ba748af8672ff3205ae6792167
asadhameed87 <leasad83@gmail.com>
2021-11-22 18:26:46 +0500

    Order details screen done, multiple items render issue till persists.

--- resources/screens/OrderDetails/OrderDetails.js
+++ resources/screens/OrderDetails/OrderDetails.js
@@ -35,6 +35,13 @@ import AwesomeAlert from 'react-native-awesome-alerts';
 import {HelperText, TextInput} from 'react-native-paper';
 import NetInfo from '@react-native-community/netinfo';
 import Accordion from 'react-native-collapsible/Accordion';
+import moment from 'moment';
+
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import Feather from 'react-native-vector-icons/Feather';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Foundation from 'react-native-vector-icons/Foundation';
 
 export default function OrderDetails({navigation}) {
   const [activeSession, setActiveSession] = useState([]);
@@ -54,7 +61,7 @@ export default function OrderDetails({navigation}) {
     fetch(url)
       .then(data => data?.json())
       .then(res => {
-        console.log(54, res);
+        // console.log(54, res);
         if (res) {
           setOrderData(res);
         } else {
@@ -87,7 +94,7 @@ export default function OrderDetails({navigation}) {
             marginTop: hp(Platform?.OS == 'ios' ? '6' : '3'),
             marginLeft: wp('3'),
           }}>
-          Orders Details
+          My Orders
         </Text>
         <Ionicons
           name="cart"
@@ -107,6 +114,7 @@ export default function OrderDetails({navigation}) {
     return (
       <ScrollView
         showsVerticalScrollIndicator={false}
+        contentContainerStyle={{paddingBottom: hp('10')}}
         style={styles.scrollViewStyle}>
         {orderData?.length > 0 ? (
           <Accordion
@@ -127,14 +135,389 @@ export default function OrderDetails({navigation}) {
   const renderHeader = item => {
     return (
       <View style={{...styles.parentCardStyle}}>
-        <Text>{item?.id}</Text>
+        <View style={styles.parentCardTopTag}>
+          <Text style={styles.parentCardTopTagText}>Order Details</Text>
+        </View>
+        <View style={styles.parentCardIconHolder}>
+          <AntDesign
+            name="shoppingcart"
+            color={color.themColorPrimary}
+            size={hp('3')}
+            style={styles.iconStyle}
+          />
+          <View style={styles.parentCardRow}>
+            <Text style={styles.parentCarddTextStyle}>Order ID:</Text>
+            <Text style={styles.parentCarddTextStyle}>
+              {item?.order_number}
+            </Text>
+          </View>
+        </View>
+        <View style={styles.parentCardIconHolder}>
+          <AntDesign
+            name="calendar"
+            color={color.themColorPrimary}
+            size={hp('3')}
+          />
+          <View style={{...styles.parentCardRow, marginTop: hp('0.7')}}>
+            <Text style={styles.parentCarddTextStyle}>Date:</Text>
+            <Text style={styles.parentCarddTextStyle}>
+              {moment(item?.created_at)?.format('Do-MMM-YYYY')}
+            </Text>
+          </View>
+        </View>
+        <View style={styles.parentCardIconHolder}>
+          <AntDesign
+            name="codesquareo"
+            color={color.themColorPrimary}
+            size={hp('3')}
+          />
+          <View style={{...styles.parentCardRow, marginTop: hp('0.7')}}>
+            <Text style={styles.parentCarddTextStyle}>Status:</Text>
+            <Text style={styles.parentCarddTextStyle}>{item?.status}</Text>
+          </View>
+        </View>
+        <View style={styles.parentCardIconHolder}>
+          <Ionicons
+            name="cash-outline"
+            color={color.themColorPrimary}
+            size={hp('3')}
+          />
+          <View
+            style={{
+              ...styles.parentCardRow,
+              marginTop: hp('0.7'),
+
+              borderBottomWidth: hp('0'),
+            }}>
+            <Text style={styles.parentCarddTextStyle}>Total:</Text>
+            <Text style={styles.parentCarddTextStyle}>
+              ${item?.grand_total} For {item?.get_order_items[0]?.quantity} Item
+            </Text>
+          </View>
+        </View>
       </View>
     );
   };
+
+  const returnItemAttributes = item => {
+    var data;
+    if (
+      item?.get_order_items[0]?.attributes !== undefined &&
+      item?.get_order_items[0]?.attributes?.length > 0
+    ) {
+      data = item?.get_order_items[0]?.attributes;
+      return data;
+    }
+  };
+
+  const renderMultipleItems = item => {
+    var data;
+    if (item?.get_order_items?.length > 0) {
+      // console.log(215, item?.get_order_items);
+      data = item?.get_order_items;
+      return data;
+    }
+  };
+
   const renderContent = item => {
     return (
-      <View style={styles.childCardStyle}>
-        <Text>{item?.id}</Text>
+      <View
+        style={{
+          ...styles.parentCardStyle,
+          marginBottom: hp('2'),
+          marginTop: hp('2'),
+          backgroundColor: '#FFEEE3',
+        }}>
+        <View style={styles.parentCardTopTag}>
+          <Text style={styles.parentCardTopTagText}>Product Details</Text>
+        </View>
+        {/* <Text>A</Text> */}
+        {renderMultipleItems(item)?.map((res, i) => {
+          // console.log(241, item);
+          // console.log(234, res, i);
+          return (
+            <View style={styles.parentCardIconHolder}>
+              <Feather
+                name="shopping-bag"
+                color={color.themColorPrimary}
+                size={hp('3')}
+                style={styles.iconStyle}
+              />
+              <View style={styles.parentCardRow}>
+                <View style={styles.viewForTextWidth}>
+                  <Text style={styles.parentCarddTextStyle}>
+                    {res?.get_products?.name} x {res?.quantity}
+                  </Text>
+                </View>
+                <View
+                  style={{...styles.viewForTextWidth, alignItems: 'flex-end'}}>
+                  <Text style={styles.parentCarddTextStyle}>
+                    {res?.get_products?.is_discounted == 2
+                      ? res?.get_products?.discounted_price
+                      : res?.get_products?.price}{' '}
+                    x{res?.quantity}
+                  </Text>
+                </View>
+              </View>
+            </View>
+          );
+        })}
+
+        <View style={styles.parentCardIconHolder}>
+          <AntDesign
+            name="fork"
+            color={color.themColorPrimary}
+            size={hp('3')}
+            style={styles.iconStyle}
+          />
+          <View style={styles.parentCardRow}>
+            <View style={styles.viewForTextWidth}>
+              <Text style={styles.parentCarddTextStyle}>
+                Product Attributes
+              </Text>
+            </View>
+            <View style={{...styles.viewForTextWidth, alignItems: 'flex-end'}}>
+              {returnItemAttributes(item)?.map(res => {
+                return (
+                  <Text
+                    style={{
+                      ...styles.parentCarddTextStyle,
+                    }}>
+                    ({res})
+                  </Text>
+                );
+              })}
+            </View>
+          </View>
+        </View>
+        <View style={styles.parentCardIconHolder}>
+          <AntDesign
+            name="calculator"
+            color={color.themColorPrimary}
+            size={hp('3')}
+            style={styles.iconStyle}
+          />
+          <View style={styles.parentCardRow}>
+            <View style={styles.viewForTextWidth}>
+              <Text style={styles.parentCarddTextStyle}>Sub-Total:</Text>
+            </View>
+            <View style={{...styles.viewForTextWidth, alignItems: 'flex-end'}}>
+              <Text
+                style={{
+                  ...styles.parentCarddTextStyle,
+                  // marginLeft: wp('2'),
+                }}>
+                {item?.get_order_items[0]?.total_amount}
+              </Text>
+            </View>
+          </View>
+        </View>
+        <View style={styles.parentCardIconHolder}>
+          <AntDesign
+            name="creditcard"
+            color={color.themColorPrimary}
+            size={hp('3')}
+            style={styles.iconStyle}
+          />
+          <View style={styles.parentCardRow}>
+            <View style={styles.viewForTextWidth}>
+              <Text style={styles.parentCarddTextStyle}>Payment Method:</Text>
+            </View>
+            <View style={{...styles.viewForTextWidth, alignItems: 'flex-end'}}>
+              <Text
+                style={{
+                  ...styles.parentCarddTextStyle,
+                  // marginLeft: wp('2'),
+                }}>
+                {item?.payment_method}
+              </Text>
+            </View>
+          </View>
+        </View>
+        <View style={styles.parentCardIconHolder}>
+          <Ionicons
+            name="cash-outline"
+            color={color.themColorPrimary}
+            size={hp('3')}
+            style={styles.iconStyle}
+          />
+          <View style={styles.parentCardRow}>
+            <View style={styles.viewForTextWidth}>
+              <Text style={styles.parentCarddTextStyle}>Total:</Text>
+            </View>
+            <View style={{...styles.viewForTextWidth, alignItems: 'flex-end'}}>
+              <Text
+                style={{
+                  ...styles.parentCarddTextStyle,
+                  // marginLeft: wp('2'),
+                }}>
+                {item?.grand_total}
+              </Text>
+            </View>
+          </View>
+        </View>
+        <View style={{...styles.parentCardTopTag}}>
+          <Text style={styles.parentCardTopTagText}>Shipping Details</Text>
+        </View>
+        <View style={styles.parentCardIconHolder}>
+          <AntDesign
+            name="user"
+            color={color.themColorPrimary}
+            size={hp('3')}
+            style={styles.iconStyle}
+          />
+          <View style={styles.parentCardRow}>
+            <View style={styles.viewForTextWidth}>
+              <Text style={styles.parentCarddTextStyle}>
+                Shipping Fullname:
+              </Text>
+            </View>
+            <View style={{...styles.viewForTextWidth, alignItems: 'flex-end'}}>
+              <Text
+                style={{
+                  ...styles.parentCarddTextStyle,
+                  // marginLeft: wp('2'),
+                }}>
+                {item?.shipping_fullname}
+              </Text>
+            </View>
+          </View>
+        </View>
+        <View style={styles.parentCardIconHolder}>
+          <AntDesign
+            name="home"
+            color={color.themColorPrimary}
+            size={hp('3')}
+            style={styles.iconStyle}
+          />
+          <View style={styles.parentCardRow}>
+            <View style={styles.viewForTextWidth}>
+              <Text style={styles.parentCarddTextStyle}>Shipping Address:</Text>
+            </View>
+            <View style={{...styles.viewForTextWidth, alignItems: 'flex-end'}}>
+              <Text
+                style={{
+                  ...styles.parentCarddTextStyle,
+                  // marginLeft: wp('2'),
+                }}>
+                {item?.shipping_address}
+              </Text>
+            </View>
+          </View>
+        </View>
+        <View style={styles.parentCardIconHolder}>
+          <MaterialCommunityIcons
+            name="city-variant-outline"
+            color={color.themColorPrimary}
+            size={hp('3')}
+            style={styles.iconStyle}
+          />
+          <View style={styles.parentCardRow}>
+            <View style={styles.viewForTextWidth}>
+              <Text style={styles.parentCarddTextStyle}>Shipping City:</Text>
+            </View>
+            <View style={{...styles.viewForTextWidth, alignItems: 'flex-end'}}>
+              <Text
+                style={{
+                  ...styles.parentCarddTextStyle,
+                  // marginLeft: wp('2'),
+                }}>
+                {item?.shipping_city}
+              </Text>
+            </View>
+          </View>
+        </View>
+        <View style={styles.parentCardIconHolder}>
+          <MaterialCommunityIcons
+            name="home-city-outline"
+            color={color.themColorPrimary}
+            size={hp('3')}
+            style={styles.iconStyle}
+          />
+          <View style={styles.parentCardRow}>
+            <View style={styles.viewForTextWidth}>
+              <Text style={styles.parentCarddTextStyle}>Shipping State:</Text>
+            </View>
+            <View style={{...styles.viewForTextWidth, alignItems: 'flex-end'}}>
+              <Text
+                style={{
+                  ...styles.parentCarddTextStyle,
+                  // marginLeft: wp('2'),
+                }}>
+                {item?.shipping_state}
+              </Text>
+            </View>
+          </View>
+        </View>
+        <View style={styles.parentCardIconHolder}>
+          <Ionicons
+            name="code-slash"
+            color={color.themColorPrimary}
+            size={hp('3')}
+            style={styles.iconStyle}
+          />
+          <View style={styles.parentCardRow}>
+            <View style={styles.viewForTextWidth}>
+              <Text style={styles.parentCarddTextStyle}>Shipping Zipcode:</Text>
+            </View>
+            <View style={{...styles.viewForTextWidth, alignItems: 'flex-end'}}>
+              <Text
+                style={{
+                  ...styles.parentCarddTextStyle,
+                  // marginLeft: wp('2'),
+                }}>
+                {item?.shipping_zipcode}
+              </Text>
+            </View>
+          </View>
+        </View>
+        <View style={styles.parentCardIconHolder}>
+          <Feather
+            name="phone"
+            color={color.themColorPrimary}
+            size={hp('3')}
+            style={styles.iconStyle}
+          />
+          <View style={styles.parentCardRow}>
+            <View style={styles.viewForTextWidth}>
+              <Text style={styles.parentCarddTextStyle}>Shipping Phone:</Text>
+            </View>
+            <View style={{...styles.viewForTextWidth, alignItems: 'flex-end'}}>
+              <Text
+                style={{
+                  ...styles.parentCarddTextStyle,
+                  // marginLeft: wp('2'),
+                }}>
+                {item?.shipping_phone}
+              </Text>
+            </View>
+          </View>
+        </View>
+        {item?.notes !== null && item?.notes !== '' && (
+          <View style={styles.parentCardIconHolder}>
+            <Foundation
+              name="clipboard-notes"
+              color={color.themColorPrimary}
+              size={hp('3')}
+              style={{...styles.iconStyle, marginLeft: wp('1')}}
+            />
+            <View style={styles.parentCardRow}>
+              <View style={styles.viewForTextWidth}>
+                <Text style={styles.parentCarddTextStyle}>Shipping Notes:</Text>
+              </View>
+              <View
+                style={{...styles.viewForTextWidth, alignItems: 'flex-end'}}>
+                <Text
+                  style={{
+                    ...styles.parentCarddTextStyle,
+                    // marginLeft: wp('2'),
+                  }}>
+                  {item?.notes}
+                </Text>
+              </View>
+            </View>
+          </View>
+        )}
       </View>
     );
   };
